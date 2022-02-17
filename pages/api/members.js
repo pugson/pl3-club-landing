3ac9e50a41cdda8a2aca3d42712af62b137692c4
Wojtek Witkowski <hi@wojtek.im>
2022-02-18 00:15:34 +0100

    functions

--- pages/api/members.js
+++ pages/api/members.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const discord = await fetch(`https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`);
   const data = discord.data;
   const members = data.approximate_member_count;
