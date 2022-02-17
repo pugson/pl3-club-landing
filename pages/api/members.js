f8e77fbecdebe97c3b12dd9001ece87a8904101a
Wojtek Witkowski <hi@wojtek.im>
2022-02-18 00:25:47 +0100

    get axios working

--- pages/api/members.js
+++ pages/api/members.js
@@ -1,6 +1,10 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
   try {
-    const discord = await fetch(`https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`);
+    const discord = await axios.get(
+      `https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`
+    );
     const data = discord.data;
     const members = data.approximate_member_count;
     const online = data.approximate_presence_count;
