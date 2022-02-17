972464e04da1f1a063d68b5e9f2e315a14faec49
Wojtek Witkowski <hi@wojtek.im>
2022-02-18 00:22:28 +0100

    functions

--- pages/api/members.js
+++ pages/api/members.js
@@ -1,8 +1,13 @@
-export default function handler(req, res) {
-  const discord = await fetch(`https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`);
-  const data = discord.data;
-  const members = data.approximate_member_count;
-  const online = data.approximate_presence_count;
+export default async function handler(req, res) {
+  try {
+    const discord = await fetch(`https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`);
+    const data = discord.data;
+    const members = data.approximate_member_count;
+    const online = data.approximate_presence_count;
 
-  res.status(200).json({ members, online });
+    res.status(200).json({ members, online });
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
 }
