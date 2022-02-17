30241ce738e0c7d36ffe255ce6c57054966ccba3
Wojtek Witkowski <hi@wojtek.im>
2022-02-17 23:48:52 +0100

    hide request behind api

---
+++ pages/api/members.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  const discord = fetch(`https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`);
+  const data = discord.data;
+  const members = data.approximate_member_count;
+  const online = data.approximate_presence_count;
+
+  res.status(200).json({ members, online });
+}
--- pages/index.js
+++ pages/index.js
@@ -39,15 +39,10 @@ export default function Home() {
   const [online, setOnline] = useState(false);
 
   useEffect(async () => {
-    const discord = await axios.get(
-      `https://discord.com/api/v9/invites/nwMzftWJt7?with_counts=true&with_expiration=true`
-    );
-    const data = discord.data;
-    const members = data.approximate_member_count;
-    const online = data.approximate_presence_count;
+    const discord = await axios.get(`/api/members`);
 
-    setMembers(members.toLocaleString());
-    setOnline(online.toLocaleString());
+    setMembers(discord.members.toLocaleString());
+    setOnline(discord.online.toLocaleString());
   }, []);
 
   return (
