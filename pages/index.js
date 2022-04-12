2266ef45acf8bf3b7ad1cd5803a49ee1f9fe75b1
Wojtek Witkowski <hi@wojtek.im>
2022-04-12 23:11:57 +0200

    add console credits

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,12 @@ export default function Home() {
 
     if (discord) setMembers(discord.members.toLocaleString());
     if (discord) setOnline(discord.online.toLocaleString());
+
+    console.info("%c\n\n\nwebsite & art by pugson.eth", "font-family:monospace; font-size: 32px");
+    console.info(
+      "%chttps://twitter.com/pugson \n\n\n",
+      "font-family:monospace; font-size: 24px; text-shadow: -1px -1px hsl(0,100%,50%), 1px 1px hsl(5.4, 100%, 50%), 3px 2px hsl(10.8, 100%, 50%), 5px 3px hsl(16.2, 100%, 50%), 7px 4px hsl(21.6, 100%, 50%), 9px 5px hsl(27, 100%, 50%), 11px 6px hsl(32.4, 100%, 50%), 13px 7px hsl(37.8, 100%, 50%), 14px 8px hsl(43.2, 100%, 50%), 16px 9px hsl(48.6, 100%, 50%), 18px 10px hsl(54, 100%, 50%), 20px 11px hsl(59.4, 100%, 50%);"
+    );
   }, []);
 
   return (
